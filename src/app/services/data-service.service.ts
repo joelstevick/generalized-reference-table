b97892b0319b463ac793b11a44f311daca944c74
Shannon Nabors <shannon.nabors@gmail.com>
2021-04-02 15:05:42 -0400

    add configuration params

--- src/app/services/data-service.service.ts
+++ src/app/services/data-service.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { ReadAllQuery } from '../config/read-all.query.type';
 
 @Injectable({
@@ -8,7 +9,7 @@ export class DataServiceService {
 
   constructor() { }
 
-  readall(query: ReadAllQuery) {
+  readall(query: ReadAllQuery, pageOptions: any, filterOptions: any, sortOptions: any) {
     return query();
   }
 
