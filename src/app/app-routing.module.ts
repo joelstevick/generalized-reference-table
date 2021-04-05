8de08c045870a5170f15a43db6b00e4ac2a64147
Joel Stevick <jstevickic-1.com@Joels-IC-1-MacBook-Pro.local>
2021-04-05 10:11:49 -0400

    refactored

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,119 +4,17 @@ import { BehaviorSubject, Subject } from 'rxjs';
 import { bocServiceProviders, ombObjectCodes } from './db';
 import { HomeComponent } from './pages/home/home.component';
 import { TableComponent } from './pages/table/table.component';
+import { bocServiceProvidersConfig } from './reference-tables/boc-service-providers/boc-service-providers.config';
+import { ombObjectCodesConfig } from './reference-tables/omb-object-codes/omb-object-codes.config';
 
-const hide = (hidden: boolean) => {
-  return hidden
-}
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   {
-    path: 'omb-object-codes', component: TableComponent, data: {
-
-      // private
-      // stores required BehaviorSubject instances
-      _context: {
-        pageRecords$: new BehaviorSubject<any[]>([]),
-        filterChanged$: new Subject()
-      },
-      // this is how data is retrieved from the (graphql) database
-      _getOmbObjectCodes: function (pageOptions, filterOptions, sortOptions) {
-        return ombObjectCodes(pageOptions, filterOptions, sortOptions);
-      },
-      // public api
-      // this function is called from the paginator when navigation events are signalled by the table
-      loadPage: function (pageOptions, filterOptions, sortOptions) {
-        const records: any[] = this._getOmbObjectCodes(pageOptions, filterOptions, sortOptions);
-
-        this._context.pageRecords$.next(records);
-      },
-      // stream that the table component listens-on
-      pageRecords$: function () {
-        return this._context.pageRecords$.asObservable();
-      },
-      // ag-grid column metadata
-      columnDefs: async () => {
-        return [
-          {
-            headerName: 'Code',
-            field: 'code'
-          },
-          {
-            headerName: 'Description',
-            field: 'description'
-          },
-          {
-            headerName: 'OMB Object Group',
-            field: 'ombObjectGroup',
-            valueFormatter: (params) => params.value ? params.value.description : null
-          },
-          {
-            headerName: 'Created By',
-            field: 'createdBy'
-          },
-          {
-            headerName: 'Updated By',
-            field: 'updatedBy'
-          },
-        ]
-      }
-    },
+    path: 'omb-object-codes', component: TableComponent, data: ombObjectCodesConfig,
   },
   {
-    path: 'boc-service-providers', component: TableComponent, data: {
-      _context: {
-        pageRecords$: new BehaviorSubject<any[]>([]),
-        filterChanged$: new Subject()
-      },
-      _getBocServiceProviders: function (pageOptions, filterOptions, sortOptions) {
-        return bocServiceProviders;;
-      },
-      loadPage: function (pageOptions, filterOptions, sortOptions) {
-        const records: any[] = this._getBocServiceProviders(pageOptions, filterOptions, sortOptions);
-
-        this._context.pageRecords$.next(records);
-      },
-      pageRecords$: function () {
-        return this._context.pageRecords$.asObservable();
-      },
-      pagination: false,
-      columnDefs: [
-        {
-          headerName: 'Number',
-          field: 'number'
-        },
-        {
-          headerName: 'Suffix',
-          field: 'suffix'
-        },
-        {
-          headerName: 'Name',
-          field: 'name'
-        },
-        {
-          headerName: 'Long Name',
-          field: 'longName'
-        },
-        {
-          headerName: 'Key Words',
-          field: 'keyWords'
-        },
-        {
-          headerName: 'Narrative',
-          field: 'narrative'
-        },
-        {
-          headerName: 'Created By',
-          field: 'createdBy',
-          hide: hide(true)
-        },
-        {
-          headerName: 'Updated By',
-          field: 'updatedBy'
-        },
-      ]
-    },
+    path: 'boc-service-providers', component: TableComponent, data: bocServiceProvidersConfig,
   }
 ];
 
