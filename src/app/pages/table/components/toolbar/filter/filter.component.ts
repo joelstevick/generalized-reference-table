fdfc8b47a3462f9d14dc0a41acc5d7c3a63ff725
Joel Stevick <jstevickic-1.com@Joels-IC-1-MacBook-Pro.local>
2021-04-12 14:12:52 -0400

    wired-up filter component to columnDefs

--- src/app/pages/table/components/toolbar/filter/filter.component.ts
+++ src/app/pages/table/components/toolbar/filter/filter.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-filter',
@@ -7,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilterComponent implements OnInit {
 
-  constructor() { }
+  constructor( public dialogRef: MatDialogRef<FilterComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { columnDefs: Record<string, any>[] }) { }
 
   ngOnInit(): void {
+    console.log(this.data);
   }
 
 }
--- src/app/pages/table/components/toolbar/toolbar.component.ts
+++ src/app/pages/table/components/toolbar/toolbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { FilterComponent } from './filter/filter.component';
 
 @Component({
   selector: 'app-toolbar',
@@ -7,18 +9,30 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 })
 export class ToolbarComponent implements OnInit {
   @Input() buttons: any;
+  @Input() columnDefs: Record<string, any>[];
+
   @Output() downloadClicked = new EventEmitter()
 
-  constructor() { }
+  constructor(
+    private dialog: MatDialog,
+    ) { }
 
   ngOnInit(): void {
   }
 
   openFilterModal() {
-    
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.data = {columnDefs: this.columnDefs};
+
+    this.dialog.open(FilterComponent, dialogConfig).afterClosed().subscribe((filter) => {
+      console.log(filter)
+      if (filter) {
+        
+      }
+    })
   }
 
   openSortModal() {
-    
+
   }
 }
