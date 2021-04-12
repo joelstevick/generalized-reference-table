8b068d40bcdf39e2dc2131ac7a7bc79fdfe8166f
Joel Stevick <jstevickic-1.com@Joels-IC-1-MacBook-Pro.local>
2021-04-12 15:19:27 -0400

    wired-up filter component to columnDefs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,6 @@ import { OmbObjectCodeDeleteComponent } from './reference-tables/omb-object-code
 import { BocServiceProviderDeleteComponent } from './reference-tables/boc-service-providers/modals/boc-service-provider-delete/boc-service-provider-delete.component';
 import { DeleteComponent } from './pages/shared-modals/delete/delete.component';
 import { FormComponent } from './pages/shared-modals/form/form.component';
-import { SortComponent } from './pages/table/components/toolbar/sort/sort.component';
 import { FilterComponent } from './pages/table/components/toolbar/filter/filter.component';
 
 @NgModule({
@@ -32,7 +31,6 @@ import { FilterComponent } from './pages/table/components/toolbar/filter/filter.
     BocServiceProviderDeleteComponent,
     DeleteComponent,
     FormComponent,
-    SortComponent,
     FilterComponent
   ],
   imports: [
